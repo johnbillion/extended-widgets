17fceb932bd33fa91c7f8f0572db6be0e4a0b643
John Blackbourn <johnbillion@gmail.com>
2015-11-28 00:02:36 +0000

    Remove use of `func_get_args()`.

--- extended-widgets.php
+++ extended-widgets.php
@@ -20,8 +20,19 @@ abstract class Extended_Widget extends WP_Widget {
 	public $args = null;
 	public $instance = null;
 
-	public function __construct() {
-		call_user_func_array( 'parent::__construct', func_get_args() );
+	/**
+	 * Constructor.
+	 *
+	 * @param string $id_base         Optional Base ID for the widget, lowercase and unique. If left empty,
+	 *                                a portion of the widget's class name will be used Has to be unique.
+	 * @param string $name            Name for the widget displayed on the configuration page.
+	 * @param array  $widget_options  Optional. Widget options. See wp_register_sidebar_widget() for information
+	 *                                on accepted arguments. Default empty array.
+	 * @param array  $control_options Optional. Widget control options. See wp_register_widget_control() for
+	 *                                information on accepted arguments. Default empty array.
+	 */
+	public function __construct( $id_base, $name, $widget_options = array(), $control_options = array() ) {
+		parent::__construct( $id_base, $name, $widget_options, $control_options );
 	}
 
 	abstract protected function process();
