f3a91eccabe2a10d7d85b37229875ad7c9c47fb1
John Blackbourn <johnbillion@gmail.com>
2016-01-25 11:32:16 +0000

    Docs!

--- extended-widgets.php
+++ extended-widgets.php
@@ -1,7 +1,7 @@
 <?php
 /*
 
-Copyright © 2012-2015 John Blackbourn
+Copyright © 2012-2016 John Blackbourn
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,8 +17,19 @@ GNU General Public License for more details.
 
 abstract class Extended_Widget extends WP_Widget {
 
-	public $args = null;
-	public $instance = null;
+	/**
+	 * Display arguments for this widget.
+	 *
+	 * @var array
+	 */
+	public $args = [];
+
+	/**
+	 * The settings for the particular instance of the widget.
+	 *
+	 * @var array
+	 */
+	public $instance = [];
 
 	/**
 	 * Constructor.
@@ -35,8 +46,18 @@ abstract class Extended_Widget extends WP_Widget {
 		parent::__construct( $id_base, $name, $widget_options, $control_options );
 	}
 
+	/**
+	 * Return the variables for use in this widget's template output.
+	 *
+	 * @return array Associative array of template variables.
+	 */
 	abstract protected function get_vars();
 
+	/**
+	 * Return the title for this widget based on the `title` instance variable.
+	 *
+	 * @return string|bool Title as a string, or boolean false if there isn't one.
+	 */
 	protected function get_title() {
 		if ( isset( $this->instance['title'] ) ) {
 			return $this->instance['title'];
@@ -44,14 +65,37 @@ abstract class Extended_Widget extends WP_Widget {
 		return false;
 	}
 
+	/**
+	 * Return the template name for this widget.
+	 *
+	 * The template name is optionally used when locating the widget's specialised template. It's functionally
+	 * identical to the `$name` parameter of `get_template_part()` and can be used to add specificity to a template.
+	 *
+	 * @return string The name of the specialised template. Empty string when not used.
+	 */
 	protected function get_template_name() {
 		return '';
 	}
 
+	/**
+	 * Whether this widget's output should have a wrapper. Controls whether the `before_widget` and `after_widget`
+	 * arguments are output around the widget.
+	 *
+	 * @return bool Should this widget's output have a wrapper?
+	 */
 	protected function has_wrapper() {
 		return true;
 	}
 
+	/**
+	 * Output the widget.
+	 *
+	 * This method controls locating the template, handling caching, and outputting the widget's contents.
+	 *
+	 * @param array $args     Display arguments including 'before_title', 'after_title',
+	 *                        'before_widget', and 'after_widget'.
+	 * @param array $instance The settings for the particular instance of the widget.
+	 */
 	final public function widget( $args, $instance ) {
 
 		$this->args     = $args;
@@ -96,6 +140,16 @@ abstract class Extended_Widget extends WP_Widget {
 
 	}
 
+	/**
+	 * Return the template slug for this widget.
+	 *
+	 * The template slug is used when locating the widget's generic template. It's functionally identical to the
+	 * `$slug` parameter of `get_template_part()`.
+	 *
+	 * By default the slug is generated from the widget name but can be overridden by a child class.
+	 *
+	 * @return string The slug name for the generic template.
+	 */
 	protected function get_template_slug() {
 
 		# This method turns "Prefix_My_Special_Widget" into "my-special". Hence, every
